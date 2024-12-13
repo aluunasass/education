f8fa2ec754d3690ee34180e06821cb37be4d9001
aluunasass <sasuna989@gmail.com>
2024-12-13 20:27:05 +0200

    Add lesson_7.py

--- lessons/lesson_7.py
+++ lessons/lesson_7.py
@@ -6,20 +6,30 @@ BOSS = "Саша"
 class Friend:
     name: str
     age: int
+    health: int
 
     def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
+        self.health = 100
 
     def __str__(self):
-        return f"Привет. Я {self.name}. Мне {self.age}."
+        return f"Привет. Я {self.name}. Мне {self.age}. Здоровье:{self.health}"
 
     def __repr__(self):
         return f"{self.name}"
 
+    def change_health(self, health: int):
+        self.health += health
+        if self.health > 100:
+            self.health = 100
+        elif self.health <= 0:
+            self.health = 0
+
     def play_volleyball(self):
         print(f"{self.name} играет с {BOSS} в волейбол")
         time.sleep(5)
+        self.change_health(15)
 
     def eat(self):
         food = input("Что будем кушать?:")
@@ -31,6 +41,7 @@ class Friend:
             print(f"{self.name} пьет пивко с {BOSS}")
         else:
             print(f"{self.name} не продали пивко")
+        self.change_health(-20)
 
     def get_options(self) -> dict:
         return {
